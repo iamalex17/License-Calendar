7ab96accd7b87d670266a2d88fefb4e865dea5f7
Alex Ungureanu <ungureanualex17@yahoo.com>
2016-05-22 13:46:44 +0000

    Latest css version & included bootstrap locally

--- calendar/resources/views/layouts/app.blade.php
+++ calendar/resources/views/layouts/app.blade.php
@@ -5,12 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Dashboard</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-        <link href="{{ URL::asset('assets/css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL('assets/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ URL('asset/assets/css/app.css') }}" rel="stylesheet">
         <link href="{{ URL('assets/calendar/fullcalendar.min.css') }}" rel="stylesheet">
         <link href="{{ URL('assets/calendar/fullcalendar.print.css') }}" rel="stylesheet" media='print'>
         <script type="text/javascript" src="{{ URL('assets/jquery.min.js') }}"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="{{ URL('assets/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ URL('assets/calendar.js') }}"></script>
         <script type="text/javascript" src="{{ URL('assets/moment.min.js') }}"></script>
         <script type="text/javascript" src="{{ URL('assets/calendar/fullcalendar.min.js') }}"></script>
@@ -28,9 +28,9 @@
                                 <button type="button" class="btn btn-link no-underline" data-toggle="modal" data-target=".modal-account">Account</button>
                             </a>
                             </li>
-                            <li><a href="#">
-                                <button type="button" class="btn btn-link no-underline" data-toggle="modal" data-target=".modal-settings">Settings</button>
-                            </a>
+                            {{-- <li><a href="#"> --}}
+                                {{-- <button type="button" class="btn btn-link no-underline" data-toggle="modal" data-target=".modal-settings">Settings</button> --}}
+                            {{-- </a> --}}
                             </li>
                             <li role="separator" class="divider"></li>
                             <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
