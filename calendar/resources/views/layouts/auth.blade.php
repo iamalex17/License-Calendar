225d066bc7a17ffc6895b8298691fb1afe10d58a
Alex Ungureanu <ungureanualex17@yahoo.com>
2016-05-22 13:48:39 +0000

    Refactored error display in register view & added bootstrap locally

--- calendar/resources/views/layouts/auth.blade.php
+++ calendar/resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Welcome to Calendar</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ URL::asset('assets/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('assets/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
